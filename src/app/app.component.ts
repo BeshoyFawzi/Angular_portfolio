c60a6167f83babbde5352bd2cec7d4a42d42098c
BeshoyFawzi <54356474+BeshoyFawzi@users.noreply.github.com>
2022-02-13 23:02:42 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+
+  // function colorFn() { 
+  //   var o=document.getElementById("myDIV").style.backgroundColor;
+  //   console.log(o)
+  // if(o==="black"){
+  //   document.getElementById("myDIV").style.backgroundColor ="white"
+  //   document.getElementById("aside-right").style.backgroundColor ="white";
+  //   document.getElementById("aside-left").style.backgroundColor ="white";
+  //   document.getElementById("About").style.backgroundColor ="white";
+  //   document.getElementById("Portfolio").style.backgroundColor ="white";
+  //   document.getElementById("Skills").style.backgroundColor ="white";
+  //   document.getElementById("Resume").style.backgroundColor ="white";
+  //   document.getElementById("Contact").style.backgroundColor ="white";
+  //   document.getElementById("About").classList.remove("pcolor")
+  //   document.getElementById("Contact").classList.remove("pcolor")
+   
+    
+  // }else{
+  //   document.getElementById("myDIV").style.backgroundColor ="black";
+  //   document.getElementById("aside-right").style.backgroundColor ="black";
+  //   document.getElementById("aside-left").style.backgroundColor ="black";
+  //   document.getElementById("About").style.backgroundColor ="black";
+  //   document.getElementById("Portfolio").style.backgroundColor ="black";
+  //   document.getElementById("Skills").style.backgroundColor ="black";
+  //   document.getElementById("Resume").style.backgroundColor ="black";
+  //   document.getElementById("Contact").style.backgroundColor ="black";
+  //   document.getElementById("About").classList.add("pcolor")
+  //   document.getElementById("Contact").classList.add("pcolor")
+    
+  // }
+   
+  // }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsideLeftComponent } from './components/aside-left/aside-left.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideLeftComponent,
+    AboutComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
